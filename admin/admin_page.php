c4fe5ca345b7c887ad13fdba0afaf750f047a01c
Gilbert Lorentz <gilbertlorentz@gmail.com>
2022-11-24 21:31:39 +0700

    Admin Page

---
+++ admin/admin_page.php
@@ -0,0 +1,224 @@
+<?php
+    require('../connect.php');
+    session_start();
+    if (!isset($_SESSION["login_admin"])) {
+        header("Location: login_admin.php");
+    }
+
+    $qry = "SELECT name FROM admin_acc 
+    WHERE username LIKE '".$_SESSION['username']."';";
+    $data_nama = query($qry);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Workspace</title>
+    <link rel="stylesheet" href="../MDB5/css/mdb.min.css" />
+    <script type="text/javascript" src="../MDB5/js/mdb.min.js"></script>
+    <link rel="stylesheet" href="../assets/fontawesome/css/all.css">
+
+</head>
+<body>
+    <!-- Navbar-->
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+        <!-- Container wrapper -->
+        <div class="container-fluid">
+            <!-- Toggle button -->
+            <!-- <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button> -->
+
+            <!-- Collapsible wrapper -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Navbar brand -->
+                <a class="navbar-brand mt-2 mt-lg-0 ms-1" href="#">
+                    <img src="white.png" height="45" alt="Gudang Resep Logo" loading="lazy"/>
+                    <!-- <small>Admin Workspace</small>
+                    <img src="Gudang Resep.png" height="45" alt="Gudang Resep Logo" loading="lazy"/> -->
+                </a>
+
+                <!-- Left links -->
+                <ul class="navbar-nav me-auto mb-0 ms-3 mb-lg-0">
+                    <li class="nav-item ms-3">
+                        <a class="nav-link active" href="#">Live Website</a>
+                    </li>
+                    <li class="nav-item ms-4">
+                        <a class="nav-link active" href="#">Admin Dashboard</a>
+                    </li>
+                </ul>
+                <!-- Left links -->
+
+                
+                <!-- Right links -->
+                <li class="nav-item dropdown mb-2 me-3">
+                    <a
+                    class="nav-link dropdown-toggle d-flex align-items-center"
+                    href="#"
+                    id="navbarDropdownMenuLink"
+                    role="button"
+                    data-mdb-toggle="dropdown"
+                    aria-expanded="false"
+                    >
+                    <img
+                        src="ipen.jpg"
+                        class="rounded-circle"
+                        height="35"
+                        alt="Profile"
+                        loading="lazy"
+                    />
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
+                        <li>
+                            <a class="dropdown-item" href="#">My profile</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#">Settings</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="logout_admin.php">Logout</a>
+                        </li>
+                    </ul>
+                </li>
+                <!-- Right links -->
+            </div>
+        </div>
+    </nav>
+    <!-- Navbar -->
+
+    <!-- Content -->
+    <div class="container-fluid bg-secondary text-white">
+        <!-- Search Bar -->
+        <div class="d-flex justify-content-between py-3 ps-3 text-dark">
+            <h3>Hello <?= $data_nama[0]['name']; ?>, welcome to Admin Dashboard</h3>
+
+            <div class="input-group w-25 d-flex justify-content-between">
+                <div class="form-outline">
+                    <input id="search-focus" type="search" id="form1" class="form-control" />
+                    <label class="form-label" for="form1">Search</label>
+                </div>
+                <button type="button" class="btn btn-primary">
+                    <i class="fas fa-search"></i>
+                </button>
+            </div>
+        </div>
+
+        <!-- Table -->
+        <table class="table align-middle mb-0 bg-secondary text-white">
+            <thead class="bg-secondary text-white">
+                <tr>
+                <th>Name</th>
+                <th>Title</th>
+                <th>Status</th>
+                <th>Position</th>
+                <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <td>
+                    <div class="d-flex align-items-center">
+                    <img
+                        src="https://mdbootstrap.com/img/new/avatars/8.jpg"
+                        alt=""
+                        style="width: 45px; height: 45px"
+                        class="rounded-circle"
+                        />
+                    <div class="ms-3">
+                        <p class="fw-bold mb-1">John Doe</p>
+                        <p class="text-muted mb-0">john.doe@gmail.com</p>
+                    </div>
+                    </div>
+                </td>
+                <td>
+                    <p class="fw-normal mb-1">Software engineer</p>
+                    <p class="text-muted mb-0">IT department</p>
+                </td>
+                <td>
+                    <span class="badge badge-success rounded-pill d-inline">Active</span>
+                </td>
+                <td>Senior</td>
+                <td>
+                    <button type="button" class="btn btn-link btn-sm btn-rounded">
+                    Edit
+                    </button>
+                </td>
+                </tr>
+                <tr>
+                <td>
+                    <div class="d-flex align-items-center">
+                    <img
+                        src="https://mdbootstrap.com/img/new/avatars/6.jpg"
+                        class="rounded-circle"
+                        alt=""
+                        style="width: 45px; height: 45px"
+                        />
+                    <div class="ms-3">
+                        <p class="fw-bold mb-1">Alex Ray</p>
+                        <p class="text-muted mb-0">alex.ray@gmail.com</p>
+                    </div>
+                    </div>
+                </td>
+                <td>
+                    <p class="fw-normal mb-1">Consultant</p>
+                    <p class="text-muted mb-0">Finance</p>
+                </td>
+                <td>
+                    <span class="badge badge-primary rounded-pill d-inline"
+                        >Onboarding</span
+                    >
+                </td>
+                <td>Junior</td>
+                <td>
+                    <button
+                            type="button"
+                            class="btn btn-link btn-rounded btn-sm fw-bold"
+                            data-mdb-ripple-color="dark"
+                            >
+                    Edit
+                    </button>
+                </td>
+                </tr>
+                <tr>
+                <td>
+                    <div class="d-flex align-items-center">
+                    <img
+                        src="https://mdbootstrap.com/img/new/avatars/7.jpg"
+                        class="rounded-circle"
+                        alt=""
+                        style="width: 45px; height: 45px"
+                        />
+                    <div class="ms-3">
+                        <p class="fw-bold mb-1">Kate Hunington</p>
+                        <p class="text-muted mb-0">kate.hunington@gmail.com</p>
+                    </div>
+                    </div>
+                </td>
+                <td>
+                    <p class="fw-normal mb-1">Designer</p>
+                    <p class="text-muted mb-0">UI/UX</p>
+                </td>
+                <td>
+                    <span class="badge badge-warning rounded-pill d-inline">Awaiting</span>
+                </td>
+                <td>Senior</td>
+                <td>
+                    <button
+                            type="button"
+                            class="btn btn-link btn-rounded btn-sm fw-bold"
+                            data-mdb-ripple-color="dark"
+                            >
+                    Edit
+                    </button>
+                </td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
+</body>
+</html>
---
+++ admin/login_admin.php
@@ -0,0 +1,189 @@
+<?php
+session_start();
+require("../connect.php");
+
+if (isset($_SESSION["login_admin"])) {
+    header("Location: admin_page.php");
+}
+
+if (isset($_POST["submit"])) {
+    $username = $_POST["loginName"];
+    $password = $_POST["loginPassword"];
+    $result = $conn->query("SELECT * FROM admin_acc WHERE username = '$username';");
+    // $result = mysqli_query($database, "SELECT * FROM user WHERE username = '$username';");
+    // print_r($result);
+    if (mysqli_num_rows($result) !== 1) {
+        $eror = true;
+        echo "
+            <script>
+            alert('eror');
+            </script>";
+        // header("Location: login2.php");
+        exit;
+    }
+
+    $row = mysqli_fetch_assoc($result);
+    // echo "<br>";
+    // print_r($row);
+
+    if ($password !== $row["password"]) {
+        $_POST["eror"] = true;
+        $eror = true;
+        echo "
+            <script>
+            alert('password salah');
+            </script>";
+        // header("Location: login2.php");
+        exit;
+    }
+
+    // echo "hello world";
+    $_SESSION["login_admin"] = true;
+    $_SESSION["username"] = $username;
+    // cek remember
+    // if(isset($_POST["remember"])) {
+    //     setcookie("id", $row["id"], time() + 60);
+    //     setcookie("key", hash("sha256", $row["username"]), time() + 60);
+    // }
+    header("Location: admin_page.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"> -->
+    <!-- JavaScript Bundle with Popper -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script> -->
+    <title>Login</title>
+    <link rel="stylesheet" href="../MDB5/css/mdb.min.css" />
+    <script type="text/javascript" src="../MDB5/js/mdb.min.js"></script>
+
+    <style>
+        .login {
+            width: 30%;
+            
+        }
+    </style>
+</head>
+
+<body>
+    <?php if (isset($_POST["eror"])) : ?>
+        <p>eror</p>
+    <?php endif; ?>
+    <div class="d-flex justify-content-center align-items-center">
+        <div class="login">
+            <!-- Pills navs -->
+            <ul class="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link active" id="tab-login" data-mdb-toggle="pill" href="#pills-login" role="tab" aria-controls="pills-login" aria-selected="true">Login</a>
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link" id="tab-register" data-mdb-toggle="pill" href="#pills-register" role="tab" aria-controls="pills-register" aria-selected="false">Register</a>
+                </li>
+            </ul>
+            <!-- Pills navs -->
+
+            <!-- Pills content -->
+            <div class="tab-content">
+                <div class="tab-pane fade show active" id="pills-login" role="tabpanel" aria-labelledby="tab-login">
+                    <form method="POST">
+                        <div class="text-center mb-3">
+                            <p>Sign in with:</p>
+                            <!-- Email input -->
+                            <div class="form-outline mb-4">
+                                <input type="text" id="loginName" name="loginName" class="form-control" />
+                                <label class="form-label" for="loginName">Email or username</label>
+                            </div>
+
+                            <!-- Password input -->
+                            <div class="form-outline mb-4">
+                                <input type="password" id="loginPassword" name="loginPassword" class="form-control" />
+                                <label class="form-label" for="loginPassword">Password</label>
+                            </div>
+
+                            <!-- 2 column grid layout -->
+                            <div class="row mb-4">
+                                <div class="col-md-6 d-flex justify-content-center">
+                                    <!-- Checkbox -->
+                                    <div class="form-check mb-3 mb-md-0">
+                                        <input class="form-check-input" type="checkbox" value="" id="loginCheck" checked />
+                                        <label class="form-check-label" for="loginCheck"> Remember me </label>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6 d-flex justify-content-center">
+                                    <!-- Simple link -->
+                                    <a href="#!">Forgot password?</a>
+                                </div>
+                            </div>
+
+                            <!-- Submit button -->
+                            <button type="submit" name="submit" class="btn btn-primary btn-block mb-4">Sign in</button>
+
+                            <!-- Register buttons -->
+                            <div class="text-center">
+                                <p>Not a member? <a href="#pills-register">Register</a></p>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="tab-pane fade" id="pills-register" role="tabpanel" aria-labelledby="tab-register">
+                    <form action="registrasi.php" method="POST">
+                        <div class="text-center mb-3">
+                            <p>Sign up with:</p>
+
+                            <!-- Name input -->
+                            <div class="form-outline mb-4">
+                                <input type="text" id="registerName" name="registerName" class="form-control" />
+                                <label class="form-label" for="registerName">Name</label>
+                            </div>
+
+                            <!-- Username input -->
+                            <div class="form-outline mb-4">
+                                <input type="text" id="registerUsername" name="registerUsername" class="form-control" />
+                                <label class="form-label" for="registerUsername">Username</label>
+                            </div>
+
+                            <!-- Email input -->
+                            <div class="form-outline mb-4">
+                                <input type="email" id="registerEmail" name="registerEmail" class="form-control" />
+                                <label class="form-label" for="registerEmail">Email</label>
+                            </div>
+
+                            <!-- Password input -->
+                            <div class="form-outline mb-4">
+                                <input type="password" id="registerPassword" name="registerPassword" class="form-control" />
+                                <label class="form-label" for="registerPassword">Password</label>
+                            </div>
+
+                            <!-- Repeat Password input -->
+                            <div class="form-outline mb-4">
+                                <input type="password" id="registerRepeatPassword" name="registerRepeatPassword" class="form-control" />
+                                <label class="form-label" for="registerRepeatPassword">Repeat password</label>
+                            </div>
+
+                            <!-- Checkbox -->
+                            <div class="form-check d-flex justify-content-center mb-4">
+                                <input class="form-check-input me-2" type="checkbox" value="" id="registerCheck" checked aria-describedby="registerCheckHelpText" />
+                                <label class="form-check-label" for="registerCheck">
+                                    I have read and agree to the terms
+                                </label>
+                            </div>
+
+                            <!-- Submit button -->
+                            <button type="submit" class="btn btn-primary btn-block mb-3">Sign in</button>
+                    </form>
+                </div>
+            </div>
+            <!-- Pills content -->
+        </div>
+    </div>
+</body>
+
+</html>
