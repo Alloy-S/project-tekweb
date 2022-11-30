2ecb72973c8a27f8664fb3432cdda52dee06700a
Gilbert Lorentz <gilbertlorentz@gmail.com>
2022-11-30 16:27:01 +0700

    connect admin to database kata ipen

--- admin/index.php
+++ admin/index.php
@@ -8,6 +8,8 @@
     $qry = "SELECT name FROM admin_acc 
     WHERE username LIKE '".$_SESSION['username']."';";
     $data_nama = query($qry);
+
+    $data = query("SELECT * FROM resep WHERE is_approved = 0");
 ?>
 
 <!DOCTYPE html>
@@ -111,7 +113,7 @@
         <table class="table align-middle mb-0 bg-secondary text-white">
             <thead class="bg-secondary text-white">
                 <tr>
-                <th>Name</th>
+                <th>Daftar Nama</th>
                 <th>Title</th>
                 <th>Status</th>
                 <th>Position</th>
@@ -119,103 +121,43 @@
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                <td>
-                    <div class="d-flex align-items-center">
-                    <img
-                        src="https://mdbootstrap.com/img/new/avatars/8.jpg"
-                        alt=""
-                        style="width: 45px; height: 45px"
-                        class="rounded-circle"
-                        />
-                    <div class="ms-3">
-                        <p class="fw-bold mb-1">Ipen</p>
-                        <p class="text-muted mb-0">john.doe@gmail.com</p>
-                    </div>
-                    </div>
-                </td>
-                <td>
-                    <p class="fw-normal mb-1">Software engineer</p>
-                    <p class="text-muted mb-0">IT department</p>
-                </td>
-                <td>
-                    <span class="badge badge-success rounded-pill d-inline">Active</span>
-                </td>
-                <td>Senior</td>
-                <td>
-                    <button type="button" class="btn btn-link btn-sm btn-rounded">
-                    Edit
-                    </button>
-                </td>
-                </tr>
-                <tr>
-                <td>
-                    <div class="d-flex align-items-center">
-                    <img
-                        src="https://mdbootstrap.com/img/new/avatars/6.jpg"
-                        class="rounded-circle"
-                        alt=""
-                        style="width: 45px; height: 45px"
-                        />
-                    <div class="ms-3">
-                        <p class="fw-bold mb-1">Alex Ray</p>
-                        <p class="text-muted mb-0">alex.ray@gmail.com</p>
-                    </div>
-                    </div>
-                </td>
-                <td>
-                    <p class="fw-normal mb-1">Consultant</p>
-                    <p class="text-muted mb-0">Finance</p>
-                </td>
-                <td>
-                    <span class="badge badge-primary rounded-pill d-inline"
-                        >Onboarding</span
-                    >
-                </td>
-                <td>Junior</td>
-                <td>
-                    <button
-                            type="button"
-                            class="btn btn-link btn-rounded btn-sm fw-bold"
-                            data-mdb-ripple-color="dark"
-                            >
-                    Edit
-                    </button>
-                </td>
-                </tr>
-                <tr>
-                <td>
-                    <div class="d-flex align-items-center">
-                    <img
-                        src="https://mdbootstrap.com/img/new/avatars/7.jpg"
-                        class="rounded-circle"
-                        alt=""
-                        style="width: 45px; height: 45px"
-                        />
-                    <div class="ms-3">
-                        <p class="fw-bold mb-1">Kate Hunington</p>
-                        <p class="text-muted mb-0">kate.hunington@gmail.com</p>
-                    </div>
-                    </div>
-                </td>
-                <td>
-                    <p class="fw-normal mb-1">Designer</p>
-                    <p class="text-muted mb-0">UI/UX</p>
-                </td>
-                <td>
-                    <span class="badge badge-warning rounded-pill d-inline">Awaiting</span>
-                </td>
-                <td>Senior</td>
-                <td>
-                    <button
-                            type="button"
-                            class="btn btn-link btn-rounded btn-sm fw-bold"
-                            data-mdb-ripple-color="dark"
-                            >
-                    Edit
-                    </button>
-                </td>
-                </tr>
+                <?php foreach ($data as $row) : ?>
+                    
+                    <tr>
+                        <td style="padding-right: 50px";>
+                            <div class="d-flex align-items-center">
+                            <img
+                                src="../img/<?= $row['gambar']; ?>"
+                                alt=""
+                                style="width: 135px; height: 95px"
+                                class="rounded"
+                                />
+                            <div class="ms-3">
+                                <p class="fw-bold mb-1 h3"><?= $row['nama_resep']; ?></p>
+                                <!-- <p class="text-muted mb-0">john.doe@gmail.com</p> -->
+                            </div>
+                            </div>
+                        </td>
+                        <td>
+                            <p class="fw-normal mb-1">Software engineer</p>
+                            <p class="text-muted mb-0">IT department</p>
+                        </td>
+                        <td>
+                            <span class="badge badge-success rounded-pill d-inline">Active</span>
+                        </td>
+                        <td>Senior</td>
+                        <td>
+                            <button type="button" class="btn btn-link btn-sm btn-rounded">
+                            Edit
+                            </button>
+                        </td>
+                    </tr>
+
+                <?php endforeach; ?>
+
+
+                
+
             </tbody>
         </table>
 
