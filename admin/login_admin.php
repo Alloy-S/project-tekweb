de8b8dea3ebc3313f073f9019d7876bc86914247
Gilbert Lorentz <gilbertlorentz@gmail.com>
2022-12-08 00:02:32 +0700

    custom admin login

--- admin/login_admin.php
+++ admin/login_admin.php
@@ -52,22 +52,98 @@ if (isset($_POST["submit"])) {
     <style>
         .login {
             width: 30%;
-            
+
+        }
+
+        body {
+            margin: auto;
+            font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+            overflow: auto;
+            background: linear-gradient(315deg, rgba(231, 152, 255, 1) 3%, rgba(60, 132, 206, 1) 38%, rgba(48, 238, 226, 1) 68%, rgba(255, 170, 25, 1) 98%);
+            animation: gradient 15s ease infinite;
+            background-size: 400% 400%;
+            background-attachment: fixed;
+        }
+
+        @keyframes gradient {
+            0% {
+                background-position: 0% 0%;
+            }
+
+            50% {
+                background-position: 100% 100%;
+            }
+
+            100% {
+                background-position: 0% 0%;
+            }
+        }
+
+        .wave {
+            background: rgb(255 255 255 / 20%);
+            border-radius: 1000% 1000% 0 0;
+            position: fixed;
+            width: 200%;
+            height: 12em;
+            animation: wave 10s -3s linear infinite;
+            transform: translate3d(0, 0, 0);
+            opacity: 0.8;
+            bottom: 0;
+            left: 0;
+            z-index: -1;
+        }
+
+        .wave:nth-of-type(2) {
+            bottom: -1.25em;
+            animation: wave 18s linear reverse infinite;
+            opacity: 0.8;
+        }
+
+        .wave:nth-of-type(3) {
+            bottom: -2.5em;
+            animation: wave 20s -1s reverse infinite;
+            opacity: 0.9;
+        }
+
+        @keyframes wave {
+            2% {
+                transform: translateX(1);
+            }
+
+            25% {
+                transform: translateX(-25%);
+            }
+
+            50% {
+                transform: translateX(-50%);
+            }
+
+            75% {
+                transform: translateX(-25%);
+            }
+
+            100% {
+                transform: translateX(1);
+            }
         }
     </style>
 </head>
 
-<body class="bg-warning">
-    <?php if (isset($_POST["eror"])) : ?>
-        <p>eror</p>
+<body>
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <?php if (isset($_POST["eror"])): ?>
+    <p>eror</p>
     <?php endif; ?>
-    <div class="d-flex justify-content-center align-items-center text-white">
+    <div class="d-flex justify-content-center align-items-center">
         <div class="login">
             <h4 class="text-center mb-4 mt-5">Welcome Admin</h4>
             <!-- Pills navs -->
             <ul class="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
                 <li class="nav-item" role="presentation">
-                    <a class="nav-link active" id="tab-login" data-mdb-toggle="pill" href="#pills-login" role="tab" aria-controls="pills-login" aria-selected="true">Login</a>
+                    <a class="nav-link active" id="tab-login" data-mdb-toggle="pill" href="#pills-login" role="tab"
+                        aria-controls="pills-login" aria-selected="true">Login</a>
                 </li>
             </ul>
             <!-- Pills navs -->
@@ -93,11 +169,11 @@ if (isset($_POST["submit"])) {
                             <!-- 2 column grid layout -->
                             <div class="row mb-4">
                                 <!-- <div class="col-md-6 d-flex justify-content-center"> -->
-                                    <!-- Checkbox -->
-                                    <!-- <div class="form-check mb-3 mb-md-0"> -->
-                                        <!-- <input class="form-check-input" type="checkbox" value="" id="loginCheck" checked /> -->
-                                        <!-- <label class="form-check-label" for="loginCheck"> Remember me </label> -->
-                                    <!-- </div> -->
+                                <!-- Checkbox -->
+                                <!-- <div class="form-check mb-3 mb-md-0"> -->
+                                <!-- <input class="form-check-input" type="checkbox" value="" id="loginCheck" checked /> -->
+                                <!-- <label class="form-check-label" for="loginCheck"> Remember me </label> -->
+                                <!-- </div> -->
                                 <!-- </div> -->
 
                                 <div class="col-md-6 d-flex justify-content-center">
