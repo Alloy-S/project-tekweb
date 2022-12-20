348b791414b355c230cea4e3801e1a3ff9fca3c6
ve <c14210210@john.petra.ac.id>
2022-12-20 16:43:09 +0700

    fix comsec

--- addComment.php
+++ addComment.php
@@ -2,34 +2,29 @@
 session_start();
 include('connect.php');
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-                $author =  $_POST["name"];
+                // $author =  $_POST["name"];       
+                if(isset($_SESSION["login_user"])){
+                        $author = $_SESSION["username_user"];
+                } else {
+                        $author = "Anonymous";
+                }
                 $comment = $_POST["comment"];
                 $resep_id = $_POST["id"];
                 
                 $query = "INSERT INTO `comments`(`comment_id`, `author`, `id_resep`, `comment`, `date_created`, `reply`) VALUES ('','$author','$resep_id','$comment',NOW(),'');";
                 $result = mysqli_query($conn, $query);
                 if ($result) {
-                        $msg = "berhasil";
+                        $msg = "Comment posted";
                 } 
                 else {
-                        $msg = "gagal";
+                        $msg = "Failed to post comment";
                 }
+                echo $msg;
          
-        $query = "SELECT * FROM comments WHERE id_resep = $resep_id ORDER BY comment_id ASC";
-        $result = mysqli_query($conn, $query);
-        $output = '';
+   
         
 
-        foreach($result as $r) {
-            $output .= '
-            <div class="comment-header">By <b>' .$r["author"]. ' </b> on <i>' .$r["date_created"]. '</i> </div>
-            <div class="comment-content">' .$r["comment"]. '</div>
-            <div class="reply">
-            <button type="button" class="btn btn-default reply" id="'.$r["comment_id"].'">Reply</button>    
-            </div>
-            ';
-            echo $output;
-        }
+        
        
 }
          
--- showComment.php
+++ showComment.php
@@ -2,25 +2,66 @@
 // session_start();
 include('connect.php');
 
-$query = "SELECT * FROM comments ORDER BY comment_id ASC";
+if(isset($_POST)){
+    $resep_id = $_POST['resep_id'];
+}
+
+$query = "SELECT * FROM comments WHERE id_resep = '$resep_id' ORDER BY comment_id ASC";
 $result = mysqli_query($conn, $query);
 $output = '';
+// $result = mysqli_fetch_all($result, MYSQLI_ASSOC);
+// var_dump($result);
 
+// if (mysqli_num_rows($result) = 0){
+//     $output = ' ';
+// } else {
 
 foreach($result as $r) {
     $output .= '
-    <div class="comment-header">By <b>' .$r["author"]. ' </b> on 
-                <i>' .$r["date_created"]. '</i> </div>
+    <div class="comment-header"><b>' .$r["author"]. ' </b> on <i>' .$r["date_created"]. '</i> </div>
     <div class="comment-content">' .$r["comment"]. '</div>
     <div class="reply">
-    <button type="button" class="btn btn-default reply" id="'.$r["comment_id"].'">Reply</button>    
+    <button type="button" class="btn btn-default reply mb-2" id="'.$r["comment_id"].'" onclick="showReply()">Reply</button>    
     </div>
     ';
-}
+    // $output .= get_reply_comment($conn, $r["comment_id"]);
+};
+
 echo $output;
 
-// json_encode($output);
-// var_dump($result);
-// ?>
+
+function get_reply_comment($conn, $parent_id = 0, $marginleft = 0){
+ $query = "SELECT * FROM comments WHERE reply = '".$reply."'";
+ $output = '';
+ $result = mysqli_query($conn, $query);
+ $count = mysqli_num_rows($result);
+    if($parent_id == 0)
+    {
+    $marginleft = 0;
+    }
+    else
+    {
+    $marginleft = $marginleft + 48;
+    }
+ if($count > 0)
+ {
+  foreach($result as $r)
+  {
+   $output .= '
+   <div class="panel panel-default" style="margin-left:'.$marginleft.'px">
+    <div class="panel-heading">By <b>'.$r["author"].'</b> on <i>'.$row["date_created"].'</i></div>
+    <div class="panel-body">'.$row["comment"].'</div>
+    <div class="panel-footer" align="right"><button type="button" class="btn btn-default reply" id="'.$row["comment_id"].'">Reply</button></div>
+   </div>
+   ';
+   $output .= get_reply_comment($conn, $r["comment_id"], $marginleft);
+  }
+ }
+ return $output;
+}
+
+// // json_encode($output);
+// // <? var_dump($result); ?>
+
 
 
